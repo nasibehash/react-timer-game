f6581c5c0bcaa82fef4996a37df93d36a7b9a31d
Nasibeh Ashtari <n.ashtari@mydigipay.com>
2024-07-13 14:30:08 +0330

    Feat: Add ResultModal and TimerChallenge component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,19 @@
-import { useState, useRef } from "react";
-export default function Player() {
-  const [enteredPlayerName, setPlayerName] = useState(null);
-  // const [submitted, setSubmitted] = useState(false);
+import { useState, useRef } from 'react';
+
+export default function Player () {
+  const [ enteredPlayerName, setPlayerName ] = useState(null);
   const playerName = useRef();
 
-  function changeHandler(event) {
-    setPlayerName(event.target.value);
+  function clickHandler () {
+    setPlayerName(playerName.current.value);
+    playerName.current.value = '';
   }
 
-  function clickHandler() {
-    setPlayerName(playerName.current.value);
-    playerName.current.value = "";
-  }
   return (
     <section id="player">
-      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        {/* <input onChange={changeHandler} value={enteredPlayerName} type="text" /> */}
-        <input ref={playerName} type="text" />
+        <input ref={playerName} type="text"/>
         <button onClick={clickHandler}>Set Name</button>
       </p>
     </section>
